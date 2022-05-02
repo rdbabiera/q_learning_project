1ab6fcdccba9ee057670651fbae540504d330b0f
rdbabiera <rdbabiera@gmail.com>
2022-05-02 11:35:22 -0500

    codebase changes

--- scripts/q_learning.py
+++ scripts/q_learning.py
@@ -55,9 +55,10 @@ class QLearning(object):
 
         self.q_reward_sub = rospy.Subscriber('/q_learning/reward', QLearningReward, self.update_q_matrix)
 
-        print("about to sleep")
         rospy.sleep(5)
-        print("time to go!")
+
+        # Debugging Tools
+        self.debug = True
 
         # Initialize Q-Matrix, Constants
         self.q_matrix = np.zeros((64, 9), dtype='f')
@@ -68,43 +69,19 @@ class QLearning(object):
         self.curr_state = 0
         self.curr_action = 0
 
+        self.last_update = 0
+
         self.epsilon = 0.001
         self.alpha = 1.0
         self.gamma = 0.8
 
         # Run Q Learning Algorithm
         self.init_q_matrix()
+        self.run_q_learning()
 
 
-    def update_q_matrix(self, data):
-        # Update Q Matrix after Retrieving Reward
-        print(f"in callback, data iteration is {data.iteration_num}")
-        if data.iteration_num > self.iterations:
-        # if True:
-            next_state = 1
-            max_reward_next_state = 1
-            #for action in self.q_matrix[next_state]:
-            #    if action > max_reward_next_state:
-
-            self.q_matrix[self.curr_state, self.curr_action] += (self.alpha * max_reward_next_state)
-            self.curr_state = self.get_new_state(int(self.curr_state), self.curr_action)
-            self.iterations = data.iteration_num
-            print(f"Set Iteration to {self.iterations}")
-
-
-            q_matrix = QMatrix()
-            q_matrix.q_matrix = self.q_matrix
-            r = rospy.Rate(5)
-            for i in range(0, 5):
-                self.q_update_pub.publish(q_matrix)
-                r.sleep()
-            
-            self.select_random_action()
-        print("exiting callback")
-
-
+    # Primary Method: init_q_matrix() creates the self.q_matrix object
     def init_q_matrix(self):
-        
         # For every state (row) in the action matrix,
         #   for every column, if column is not -1, set that value in the qmatrix to 0 (represents valid action)
         for i, c in enumerate(self.action_matrix):
@@ -112,43 +89,73 @@ class QLearning(object):
                 if action != -1:
                     self.q_matrix[i, int(action)] = 0
 
-        self.select_random_action()
-        
-        # expected_iteration = 0
+    # Primary Method: run_q_learning() fills out the self.q_matrix object until 
+    # convergence
+    def run_q_learning(self):
+        expected_iteration = 0
+        while not self.q_convergence:
+            if self.iterations == expected_iteration:
+                expected_iteration += 1
+                self.select_random_action()
+        self.save_q_matrix()
+        exit(0)
 
-        # while not self.q_convergence:
-        #     print(f"expecting {expected_iteration}")
-        #     while (self.iterations != expected_iteration):
-        #         continue
-        #     # Set Lock
-        #     print(f"set lock, curr state {self.curr_state}")
-        #     expected_iteration += 1
-        # 
-        #     # Randomly Generate Action
-        #     action = np.random.randint(0, 9)
-        #     while self.q_matrix[int(self.curr_state), action] == -1:
-        #         action = np.random.randint(0, 9)
-        #     self.curr_action = action
-        #     print(f"set action to {action}")
-# 
-        #     # Create Object
-        #     command = RobotMoveObjectToTag()
-        #     command.robot_object = self.actions[self.curr_action]['object']
-        #     command.tag_id = self.actions[self.curr_action]['tag']
-        #     print(f"Command: object: {command.robot_object}, tag: {command.tag_id}")
-# 
-        #    # Publish To Topic
-        #     for i in range(0, 1):
-        #         self.q_action_pub.publish(command)
-    
+
+    # Helper Method: check_reset(state) sets state back to zero if all three 
+    # objects are not at the origin
+    def check_reset(self, state):
+        for s in self.states[state]:
+            if s == 0:
+                return False
+        return True
+
+
+    # Callback Method: update_q_matrix(data) updates the q_matrix on each robot 
+    # action followed by a reward
+    def update_q_matrix(self, data):
+        if self.debug:
+            print(f"in callback, data iteration is {data.iteration_num}")
+
+        # Update Q Matrix after Retrieving Reward
+        next_state = self.get_new_state(int(self.curr_state), self.curr_action)
+        max_reward_next_state = 0
+        for action_reward in self.q_matrix[next_state]:
+            if action_reward > max_reward_next_state:
+                max_reward_next_state = action_reward
+
+        update = (self.alpha * (data.reward + (self.gamma * max_reward_next_state) - \
+            self.q_matrix[self.curr_state, self.curr_action]))
+        self.q_matrix[self.curr_state, self.curr_action] += update
+
+
+        if self.check_reset(next_state):
+            self.curr_state = 0
+            if update < self.epsilon:
+                self.q_convergence = True
+        else:
+            self.curr_state = next_state
+
+        if self.debug:
+            print(f"Set Iteration to {self.iterations}")
+            print(f"Prize: {data.reward}")
+
+        q_matrix = QMatrix()
+        q_matrix.q_matrix = self.q_matrix
+        self.q_update_pub.publish(q_matrix)
+
+        # Release Lock
+        self.iterations += 1
+
+    # Helper Method: get_new_state(state, action) retrieves the next state 
+    # given the current state and the action taken.
     def get_new_state(self, state, action):
-        #print(f"State {state}")
-        #print(self.action_matrix[state])
         for s, a in enumerate(self.action_matrix[state]):
             if a == action:
                 return s
         return -1
 
+    # Helper Method: select_random_action() selects a random possible action 
+    # and publishes it to the robot.
     def select_random_action(self):
         # Randomly Generate Action
         action = np.random.randint(0, 9)
@@ -160,20 +167,19 @@ class QLearning(object):
         command = RobotMoveObjectToTag()
         command.robot_object = self.actions[self.curr_action]['object']
         command.tag_id = self.actions[self.curr_action]['tag']
-        print(f"Command: object: {command.robot_object}, tag: {command.tag_id}")
+
+        if self.debug:
+            print(f"From state {self.curr_state}, perform {self.curr_action}")
+            print(f"Command: object: {command.robot_object}, tag: {command.tag_id}")
 
         # Publish To Topic
-        r = rospy.Rate(5)
-        for i in range(0, 5):
-            self.q_action_pub.publish(command)
-            r.sleep()
-        print("exiting publisher")
+        self.q_action_pub.publish(command)
 
 
     def save_q_matrix(self):
-        # TODO: You'll want to save your q_matrix to a file once it is done to
+        # You'll want to save your q_matrix to a file once it is done to
         # avoid retraining
-        numpy.savetxt(qmatrix_path + "q_matrix.csv", self.q_matrix, delimiter=",")
+        np.savetxt(qmatrix_path + "q_matrix.csv", self.q_matrix, delimiter=",")
         return
 
 if __name__ == "__main__":
